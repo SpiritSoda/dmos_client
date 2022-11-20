a1777f1450b3fbd07d2b80349ac89c65eef251e4
SpiritSoda <1800013119@pku.edu.cn>
2022-11-20 09:53:56 +0800

    [Adjust] add id field

--- DMOSClientContext.java
+++ DMOSClientContext.java
@@ -2,20 +2,22 @@ package com.dmos.dmos_client;
 
 import com.google.gson.Gson;
 import io.netty.channel.Channel;
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@Data
 public class DMOSClientContext {
+    private int id;
     private Channel parent;
 
     public void channel(Channel channel){
         this.parent = channel;
     }
+    public void id(int id) { this.id = id; }
     public void send(Object o){
         if(parent != null && parent.isActive()){
-            parent.write(new Gson().toJson(o));
-            parent.flush();
-            log.info("发送消息中");
+            parent.writeAndFlush(new Gson().toJson(o));
         }
     }
 }
