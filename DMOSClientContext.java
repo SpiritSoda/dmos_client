069d0e02b5bd274978da56bcebb0d253a2792904
SpiritSoda <1800013119@pku.edu.cn>
2022-11-20 10:07:59 +0800

    [Adjust] add token field

--- DMOSClientContext.java
+++ DMOSClientContext.java
@@ -9,12 +9,17 @@ import lombok.extern.slf4j.Slf4j;
 @Data
 public class DMOSClientContext {
     private int id;
+    // 连接token，不是机器token
+    // 每个机器有自己的token，连接前向register发送token(http请求)，register会返回一个限时的连接token用于向非register机器连接
+    // 从而避免机器自己的token泄漏到非register机器
+    private String token;
     private Channel parent;
 
     public void channel(Channel channel){
         this.parent = channel;
     }
     public void id(int id) { this.id = id; }
+    public void token(String token) { this.token = token; }
     public void send(Object o){
         if(parent != null && parent.isActive()){
             parent.writeAndFlush(new Gson().toJson(o));
